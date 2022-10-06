adcc7892f8cac84680988f81c4708c4cecfe506e
reidst <reidst@hendrix.edu>
2022-10-05 20:03:35 -0500

    Terrace Hill using Python 3

---
+++ python3/terracehill.py
@@ -0,0 +1,19 @@
+input()
+terraces = list(map(int, input().split()))
+
+total = 0
+stack = []
+
+for index, terrace in enumerate(terraces):
+    
+    while len(stack) > 0 and terrace > stack[-1]['t']:
+        stack.pop()
+
+    if len(stack) > 0 and terrace == stack[-1]['t']:
+        total += index - stack[-1]['i'] - 1
+        stack.pop()
+    
+    stack.append({'i': index, 't': terrace})
+
+
+print(total)
