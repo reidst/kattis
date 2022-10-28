525f088b212526ce95c25e8e471f6cd6f091ca40
Simon <reidst@hendrix.edu>
2022-10-28 00:45:07 -0500

    Shortened Nicknames golf to 144

--- python3/nicknames_golfed.py
+++ python3/nicknames_golfed.py
@@ -1,2 +1,2 @@
-from bisect import*;l=bisect_left;i=input;e=sorted([i()for _ in[1]*int(i())])
+from bisect import*;l=bisect_left;i=input;e=sorted(map(i,['']*int(i())))
 for _ in[1]*int(i()):print(l(e,(n:=i())[:-1]+chr(1+ord(n[-1])))-l(e,n))
