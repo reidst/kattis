9704e80d918a9f84a85106fdf86a3a7426b43647
reidst <reidst@hendrix.edu>
2022-10-27 21:00:03 -0500

    Nicknames using Python 3
    Includes golfed version, 151 chars

---
+++ python3/nicknames.py
@@ -0,0 +1,15 @@
+from bisect import bisect_left
+
+A = int(input())
+names = [input() for _ in range(A)]
+names.sort()
+
+B = int(input())
+for _ in range(B):
+    nick = input()
+    i = bisect_left(names, nick)
+    j = list(nick)
+    j[-1] = chr(1 + ord(j[-1]))
+    j = ''.join(j)
+    j = bisect_left(names, j)
+    print(j - i)
