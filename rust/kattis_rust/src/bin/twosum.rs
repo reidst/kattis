706da518f8947c1858902f62dd4561945a7ab9ab
Simon <reidst@hendrix.edu>
2023-01-31 23:23:06 -0600

    First 2 Rust problems

---
+++ rust/kattis_rust/src/bin/twosum.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut line = String::new();
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut line).unwrap();
+    let sum = line.split_whitespace()
+        .map( |x| 
+            x.parse::<usize>().unwrap()
+        ).sum::<usize>();
+    println!("{}", sum);
+}
