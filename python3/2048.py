2bf620f2029d384a576c6799baa0eb3e2b84d5ec
Simon <reidst@hendrix.edu>
2023-08-10 22:34:22 -0500

    problems solved on 2023-08-10

---
+++ python3/2048.py
@@ -0,0 +1,41 @@
+board = [list(map(int, input().split())) for _ in range(4)]
+direction = int(input())
+
+
+def swipe(b, d):
+    if d in (1, 3):
+        b = transpose(b)
+    if d in (2, 3):
+        for row in b:
+            row.reverse()
+    new_b = []
+    for r in range(4):
+        new_r = []
+        squished = [n for n in b[r] if n != 0]
+        can_combine = True
+        for i, n in enumerate(squished):
+            if i > 0 and n == new_r[-1] and can_combine:
+                new_r[-1] *= 2
+                can_combine = False
+                continue
+            new_r.append(n)
+            can_combine = True
+        new_r.extend([0] * (4 - len(new_r)))
+        new_b.append(new_r)
+    if d in (2, 3):
+        for row in new_b:
+            row.reverse()
+    if d in (1, 3):
+        new_b = transpose(new_b)
+    return new_b
+
+
+def transpose(b):
+    new_b = [[None]*4 for _ in range(4)]
+    for r, row in enumerate(b):
+        for c, num in enumerate(row):
+            new_b[c][r] = num
+    return new_b
+
+
+print(*(' '.join(map(str, row)) for row in swipe(board, direction)), sep='\n')
---
+++ python3/3dprinter.py
@@ -0,0 +1,16 @@
+import math
+n = int(input())
+print(n if n < 4 else int(math.log(n - 1, 2)) + 2)
+
+# 1 -> 1; s
+# 2 -> 2; s,s
+# 3 -> 3; s,s,s
+# 4 -> 3; p,ss,ss
+# 5 -> 4; p,ss,ss,s_
+# 6 -> 4; p,ss,ss,ss
+# 7 -> 4; p,pp,ssss,sss_
+# 8 -> 4; p,pp,ssss,ssss
+# 9 -> 5; p,pp,pppp,ssssssss,s_______
+# ...
+# 16 -> 5; p,pp,pppp,ssssssss,ssssssss
+# 17 -> 6;
