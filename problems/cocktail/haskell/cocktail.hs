df7850fdf33dbb7e66f4bc4c3412d350328fb0cb
Katherine Reid <reidst@hendrix.edu>
2025-07-15 14:10:34 -0500

    fixed outdated check script

--- problems/cocktail/haskell/cocktail.hs
+++ problems/cocktail/haskell/cocktail.hs
@@ -1,6 +1,20 @@
 -- kattis-unsubmitted
+import Data.List (sort)
+
 main :: IO ()
 main = interact solve
 
 solve :: String -> String
-solve = _
+solve s = solveNums (potionTimes s) (drinkTime s)
+  
+potionTimes :: String -> [Int]
+potionTimes = reverse . sort . map read . tail . lines
+
+drinkTime :: String -> Int
+drinkTime = read . last . words . head . lines
+
+solveNums :: [Int] -> Int -> String
+solveNums []     _ = "YES"
+solveNums (x:xs) t
+  | length xs * t < x = solveNums xs t
+  | otherwise         = "NO"
